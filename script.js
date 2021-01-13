d8be7b1d02a06c017add55431604c1e3f02aacbf
Clems-web <clement.servais1@gmail.com>
2021-01-13 14:59:28 +0100

    Finito !

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
 // FIXME Assurez vous que ces deux variables contiennent bien les bonnes choses ;-)
-let player = $('.player__video').first();
-let progress = $('.progress__filled').first();
+let player = $('.player__video').get(0);
+let progress = $('.progress__filled').get(0);
+
+
+$('#play').click(function(){
+    if ($(this).hasClass("toggle")) {
+        player.play();
+        $(this).html("⏸");
+    }
+    else if (!$(this).hasClass("toggle")) {
+        player.pause();
+        $(this).html("►");
+    }
+
+    $(this).toggleClass("toggle");
+});
+
+player.ontimeupdate = function () {
+    progress.style.width = Math.floor( (100 / player.duration) * player.currentTime) + "px";
+}
+
+
